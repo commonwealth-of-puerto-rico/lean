d19ec0d998ce3aa330e20ee5cde9e151461c66fe
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-02-18 21:29:09 -0400

    Fix typo

--- paart/apps/tools/links.py
+++ paart/apps/tools/links.py
@@ -12,5 +12,5 @@ from .permissions import (PERMISSION_TOOL_PROFILE_EDIT, PERMISSION_TOOL_PROFILE_
 link_tools_profile_edit = Link(text=_(u'edit'), view='tools_profile_edit', args='resolved_object.pk', icon=icon_tools_profile_edit, permissions=[PERMISSION_TOOL_PROFILE_EDIT])
 link_tools_profile_delete = Link(text=_(u'delete'), view='tools_profile_delete', args='resolved_object.pk', icon=icon_tools_profile_delete, permissions=[PERMISSION_TOOL_PROFILE_DELETE])
 link_tools_profile_view = Link(text=_(u'details'), view='tools_profile_view', args='resolved_object.pk', icon=icon_tools_profile_view, permissions=[PERMISSION_TOOL_PROFILE_VIEW])
-link_agency_tools_profile_list = Link(text=_(u'tools_profile'), view='agency_tools_profile_list', args='resolved_object.pk', icon=icon_agency_tools_profile)
+link_agency_tools_profile_list = Link(text=_(u'tools profile'), view='agency_tools_profile_list', args='resolved_object.pk', icon=icon_agency_tools_profile)
 
