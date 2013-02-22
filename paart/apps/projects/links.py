3c3c0a815443bf2018e0e1508755f1a2b60c0dee
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-02-22 12:42:44 -0400

    Update create proyect label

--- paart/apps/projects/links.py
+++ paart/apps/projects/links.py
@@ -23,7 +23,7 @@ link_project_edit = Link(text=_(u'edit'), view='project_edit', args='resolved_ob
 link_project_delete = Link(text=_(u'delete'), view='project_delete', args='resolved_object.pk', icon=icon_project_delete, permissions=[PERMISSION_AGENCY_EDIT])
 link_project_view = Link(text=_(u'view'), view='project_view', args='resolved_object.pk', icon=icon_project_view, permissions=[PERMISSION_AGENCY_VIEW])
 link_project_view_basic = Link(text=_(u'basic'), view='project_view', args='resolved_object.pk', icon=icon_project_view, permissions=[PERMISSION_AGENCY_VIEW])
-link_project_create = Link(text=_(u'create project'), view='project_create', args='resolved_object.pk', icon=icon_project_create, permissions=[PERMISSION_AGENCY_EDIT])
+link_project_create = Link(text=_(u'add project'), view='project_create', args='resolved_object.pk', icon=icon_project_create, permissions=[PERMISSION_AGENCY_EDIT])
 
 link_project_info_edit = Link(text=_(u'edit'), view='project_info_edit', args='resolved_object.pk', icon=icon_project_info_edit, permissions=[PERMISSION_AGENCY_EDIT])
 link_project_info_delete = Link(text=_(u'delete'), view='project_info_delete', args='resolved_object.pk', icon=icon_project_info_delete, permissions=[PERMISSION_AGENCY_EDIT])
