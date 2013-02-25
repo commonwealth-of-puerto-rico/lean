1d2bfaf5032319864a1ee540ef59ac1d0576938d
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2013-02-25 15:48:05 -0400

    Disable tools menu

--- paart/apps/project_tools/__init__.py
+++ paart/apps/project_tools/__init__.py
@@ -4,4 +4,4 @@ from navigation.api import register_top_menu
 
 from .links import link_tools_menu
 
-tool_menu = register_top_menu('tools', link=link_tools_menu, position=-3)
+#tool_menu = register_top_menu('tools', link=link_tools_menu, position=-3)
