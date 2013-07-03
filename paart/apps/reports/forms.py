eeb06f2eeafa3abb5d9f73c6a8bf73df5e55cc27
Sasha Méndez <sasha.mendez@gmail.com>
2013-07-03 02:04:30 -0400

    Added fiscal year as search option

--- paart/apps/reports/forms.py
+++ paart/apps/reports/forms.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import
 from django import forms
 
 from agencies.models import Agency
+from projects.models import ProjectInfo, FiscalYear
     
 class AgencySearchForm(forms.Form):
     agency_text = forms.CharField()
-    
+    fiscal_year = forms.ModelChoiceField(queryset=FiscalYear.objects.all(), label= 'Fiscal Year')
--- paart/apps/reports/views.py
+++ paart/apps/reports/views.py
@@ -12,7 +12,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 
-from projects.models import Project
+from projects.models import Project, ProjectInfo
 
 from permissions.models import Permission
 
@@ -24,26 +24,37 @@ def agency_search_report(request):
     
     error = False
     title = 'Agency project search'
-    form = AgencySearchForm()
+    search_results = None
+    agency_search_form = AgencySearchForm()
     
     context = {
-                'form': form,
+                'agency_search_form': agency_search_form,
                 'title': title,
                 'error': error
     }
     
-    if 'q' in request.GET:
-        q = request.GET['q']
+    if 'agency_text' in request.GET:
 
-        if not q:
+        agency_text = request.GET['agency_text']
+
+        if not agency_text:
             context['error'] = True
         else:
-            context['q'] = q           
-            context['projects'] = Project.objects.filter(agency__name__icontains=q).order_by('agency__name')
-            context['title'] = title + ' for ' + q
+            context['agency_text'] = agency_text           
+            search_results = Project.objects.filter(agency__name__icontains=agency_text).order_by('agency__name')
+            context['title'] = title + ' for ' + agency_text
             
-            if not context['projects']:
+            if 'fiscal_year' in request.GET and request.GET['fiscal_year']:
+                fiscal_year = request.GET['fiscal_year']
+                
+                search_results = search_results.filter(projectinfo__fiscal_year=fiscal_year)
+                    
+            context['results'] = search_results
+            
+            if not context['results']:
                 context['message'] = 'No results'
+            else:
+                context['message'] = str(len(context['results'])) + ' results for ' + context['agency_text']
     
     return render_to_response('agency_search.html', context, context_instance=RequestContext(request))
 
