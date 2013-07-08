d86bb67da0ea173a7bea65ee8384d03c3df6ff3e
Sasha Méndez <sasha.mendez@gmail.com>
2013-07-08 03:41:59 -0400

    Added more search fields. Added limit of 100 results.

--- paart/apps/reports/forms.py
+++ paart/apps/reports/forms.py
@@ -1,10 +1,19 @@
+# -*- coding: utf-8 -*- 
+
 from __future__ import absolute_import
 
 from django import forms
 
 from agencies.models import Agency
-from projects.models import ProjectInfo, FiscalYear
+from projects.models import (ProjectInfo, FiscalYear, Purpose, Classification, 
+Methodology, Department, Goal)
     
 class AgencySearchForm(forms.Form):
-    agency_text = forms.CharField()
-    fiscal_year = forms.ModelChoiceField(queryset=FiscalYear.objects.all(), label= 'Fiscal Year')
+    agency_text = forms.CharField(label='Agencia') 
+    fiscal_year = forms.ModelChoiceField(queryset=FiscalYear.objects.all(), label= 'Año Fiscal')
+    purpose = forms.ModelChoiceField(queryset=Purpose.objects.all(), label= 'Proposito')
+    classification = forms.ModelChoiceField(queryset=Classification.objects.all(), label= 'Clasificacíon')
+    secondary_classification = forms.ModelChoiceField(queryset=Classification.objects.all(), label= 'Clasificación Secundaria')
+    methodology = forms.ModelChoiceField(queryset=Methodology.objects.all(), label= 'Metodología')
+    department = forms.ModelChoiceField(queryset=Department.objects.all(), label= 'Departamento')
+    goal = forms.ModelChoiceField(queryset=Goal.objects.all(), label= 'Meta')
--- paart/apps/reports/views.py
+++ paart/apps/reports/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 from __future__ import absolute_import
 
 import mimetypes
@@ -12,7 +14,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 
-from projects.models import Project, ProjectInfo
+from projects.models import Project, ProjectInfo, Agency
 
 from permissions.models import Permission
 
@@ -22,39 +24,52 @@ from .forms import AgencySearchForm
     
 def agency_search_report(request):
     
-    error = False
-    title = 'Agency project search'
+    title = 'Búsqueda de proyectos por agencia'
     search_results = None
     agency_search_form = AgencySearchForm()
+    kwargs = {}
     
     context = {
                 'agency_search_form': agency_search_form,
                 'title': title,
-                'error': error
     }
     
-    if 'agency_text' in request.GET:
+    if request.GET:
+        
+        if 'agency_text' in request.GET and request.GET['agency_text']:
+            kwargs['agency__name__icontains'] = request.GET['agency_text']
 
-        agency_text = request.GET['agency_text']
+        if 'fiscal_year' in request.GET and request.GET['fiscal_year']:
+            kwargs['projectinfo__fiscal_year'] = request.GET['fiscal_year']
+            
+        if 'purpose' in request.GET and request.GET['purpose']:
+            kwargs['projectinfo__purpose'] = request.GET['purpose']
+            
+        if 'classification' in request.GET and request.GET['classification']:
+            kwargs['projectinfo__classification'] = request.GET['classification']
+            
+        if 'secondary_classification' in request.GET and request.GET['secondary_classification']:
+            kwargs['projectinfo__classification_secondary'] = request.GET['secondary_classification']
+            
+        if 'methodology' in request.GET and request.GET['methodology']:
+            kwargs['projectinfo__methodology'] = request.GET['methodology']
 
-        if not agency_text:
-            context['error'] = True
+        if 'goal' in request.GET and request.GET['goal']:
+            kwargs['projectinfo__goals__in'] = request.GET['goal']
+
+        if 'department' in request.GET and request.GET['department']:
+            kwargs['projectinfo__department'] = request.GET['department']
+            
+        search_results = Project.objects.filter(**kwargs).order_by('agency__name')               
+        context['results'] = search_results[:100]
+
+        if not context['results']:
+            context['message'] = 'No results'
         else:
-            context['agency_text'] = agency_text           
-            search_results = Project.objects.filter(agency__name__icontains=agency_text).order_by('agency__name')
-            context['title'] = title + ' for ' + agency_text
-            
-            if 'fiscal_year' in request.GET and request.GET['fiscal_year']:
-                fiscal_year = request.GET['fiscal_year']
-                
-                search_results = search_results.filter(projectinfo__fiscal_year=fiscal_year)
-                    
-            context['results'] = search_results
-            
-            if not context['results']:
-                context['message'] = 'No results'
+            if len(search_results) <= 100:
+                context['message'] = str(len(context['results'])) + ' resultados '
             else:
-                context['message'] = str(len(context['results'])) + ' results for ' + context['agency_text']
-    
+                context['message'] = 'Presentando ' + str(len(context['results'])) + ' de ' + str(len(search_results)) + ' resultados, favor de utilizar un filtro.'                    
+
     return render_to_response('agency_search.html', context, context_instance=RequestContext(request))
 
