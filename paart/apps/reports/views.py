24f9190fdc396b3befc2f61e7c79c275247f125b
Sasha Méndez <sasha.mendez@gmail.com>
2013-07-08 15:31:32 -0400

    Search shows what terms were used

--- paart/apps/reports/views.py
+++ paart/apps/reports/views.py
@@ -23,53 +23,54 @@ from .permissions import (PERMISSION_PROJECT_VIEW)
 from .forms import AgencySearchForm
     
 def agency_search_report(request):
-    
     title = 'Búsqueda de proyectos por agencia'
     search_results = None
-    agency_search_form = AgencySearchForm()
     kwargs = {}
-    
-    context = {
-                'agency_search_form': agency_search_form,
-                'title': title,
-    }
+    context = {}
     
     if request.GET:
-        
-        if 'agency_text' in request.GET and request.GET['agency_text']:
-            kwargs['agency__name__icontains'] = request.GET['agency_text']
+        form = AgencySearchForm(data=request.GET)
+        if form.is_valid():
+            if 'agency_text' in request.GET and request.GET['agency_text']:
+                kwargs['agency__name__icontains'] = request.GET['agency_text']
 
-        if 'fiscal_year' in request.GET and request.GET['fiscal_year']:
-            kwargs['projectinfo__fiscal_year'] = request.GET['fiscal_year']
-            
-        if 'purpose' in request.GET and request.GET['purpose']:
-            kwargs['projectinfo__purpose'] = request.GET['purpose']
-            
-        if 'classification' in request.GET and request.GET['classification']:
-            kwargs['projectinfo__classification'] = request.GET['classification']
-            
-        if 'secondary_classification' in request.GET and request.GET['secondary_classification']:
-            kwargs['projectinfo__classification_secondary'] = request.GET['secondary_classification']
-            
-        if 'methodology' in request.GET and request.GET['methodology']:
-            kwargs['projectinfo__methodology'] = request.GET['methodology']
+            if 'fiscal_year' in request.GET and request.GET['fiscal_year']:
+                kwargs['projectinfo__fiscal_year'] = request.GET['fiscal_year']
+                
+            if 'purpose' in request.GET and request.GET['purpose']:
+                kwargs['projectinfo__purpose'] = request.GET['purpose']
+                
+            if 'classification' in request.GET and request.GET['classification']:
+                kwargs['projectinfo__classification'] = request.GET['classification']
+                
+            if 'secondary_classification' in request.GET and request.GET['secondary_classification']:
+                kwargs['projectinfo__classification_secondary'] = request.GET['secondary_classification']
+                
+            if 'methodology' in request.GET and request.GET['methodology']:
+                kwargs['projectinfo__methodology'] = request.GET['methodology']
 
-        if 'goal' in request.GET and request.GET['goal']:
-            kwargs['projectinfo__goals__in'] = request.GET['goal']
+            if 'goal' in request.GET and request.GET['goal']:
+                kwargs['projectinfo__goals__in'] = request.GET['goal']
 
-        if 'department' in request.GET and request.GET['department']:
-            kwargs['projectinfo__department'] = request.GET['department']
-            
-        search_results = Project.objects.filter(**kwargs).order_by('agency__name')               
-        context['results'] = search_results[:100]
+            if 'department' in request.GET and request.GET['department']:
+                kwargs['projectinfo__department'] = request.GET['department']
+                
+            search_results = Project.objects.filter(**kwargs).order_by('agency__name')               
+            context['results'] = search_results[:100]
 
-        if not context['results']:
-            context['message'] = 'No results'
-        else:
-            if len(search_results) <= 100:
-                context['message'] = str(len(context['results'])) + ' resultados '
+            if not context['results']:
+                context['message'] = 'No results'
             else:
-                context['message'] = 'Presentando ' + str(len(context['results'])) + ' de ' + str(len(search_results)) + ' resultados, favor de utilizar un filtro.'                    
+                if len(search_results) <= 100:
+                    context['message'] = str(len(context['results'])) + ' resultados '
+                else:
+                    context['message'] = 'Presentando ' + str(len(context['results'])) + ' de ' + str(len(search_results)) + ' resultados, favor de utilizar un filtro.'                    
+
+        context['agency_search_form'] = form
+    else:
+        context.update({
+            'agency_search_form': AgencySearchForm(),
+            'title': title,
+        })
 
     return render_to_response('agency_search.html', context, context_instance=RequestContext(request))
-
