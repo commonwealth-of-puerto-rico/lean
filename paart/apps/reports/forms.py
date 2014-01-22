f4558cd7adc5c569697711111fcbc0927bbe5e05
Sasha Méndez <sasha.mendez@gmail.com>
2014-01-22 17:21:55 -0400

    Added to report search: search by project, search by dates, pagination for results.

--- paart/apps/reports/forms.py
+++ paart/apps/reports/forms.py
@@ -8,9 +8,11 @@ from django.utils.translation import ugettext_lazy as _
 from agencies.models import Agency
 from projects.models import (ProjectInfo, FiscalYear, Purpose, Classification, 
 Methodology, Department, Goal)
-    
+
+
 class AgencySearchForm(forms.Form):
     agency_text = forms.CharField(required=False, label=_('Agency'))
+    project_text = forms.CharField(required=False, label=_('Project'))
     fiscal_year = forms.ModelChoiceField(required=False, queryset=FiscalYear.objects.all(), label=_('Fiscal Year'))
     purpose = forms.ModelChoiceField(required=False, queryset=Purpose.objects.all(), label=_('Purpose'))
     classification = forms.ModelChoiceField(required=False, queryset=Classification.objects.all(), label=_('Classification'))
@@ -18,3 +20,5 @@ class AgencySearchForm(forms.Form):
     methodology = forms.ModelChoiceField(required=False, queryset=Methodology.objects.all(), label=_('Methodology'))
     department = forms.ModelChoiceField(required=False, queryset=Department.objects.all(), label=_('Department'))
     goal = forms.ModelChoiceField(required=False, queryset=Goal.objects.all(), label=_('Goals'))
+    date_from = forms.DateField(required=False, widget=forms.TextInput(attrs={'class': 'datepicker'}), label=_('Date From'))
+    date_to = forms.DateField(required=False, widget=forms.TextInput(attrs={'class': 'datepicker'}), label=_('Date to'))
--- paart/apps/reports/views.py
+++ paart/apps/reports/views.py
@@ -2,37 +2,35 @@
 
 from __future__ import absolute_import
 
-import mimetypes
-import urllib
+import datetime
+import pytz
 
-from django.contrib import messages
-from django.core.exceptions import PermissionDenied
-from django.core.servers.basehttp import FileWrapper
-from django.core.urlresolvers import reverse, resolve
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.conf import global_settings
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _, string_concat
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from projects.models import Project, ProjectInfo, Agency
 
 from permissions.models import Permission
 
-from .permissions import (PERMISSION_PROJECT_VIEW)
-
 from .forms import AgencySearchForm
 
+
 def agency_search_report(request):
-    title = 'Búsqueda de proyectos por agencia'
-    search_results = None
     kwargs = {}
     context = {}
+    localtz = pytz.timezone(global_settings.TIME_ZONE)
 
     if request.GET:
         form = AgencySearchForm(data=request.GET)
         if form.is_valid():
             if 'agency_text' in request.GET and request.GET['agency_text']:
                 kwargs['agency__name__icontains'] = request.GET['agency_text']
+
+            if 'project_text'in request.GET and request.GET['project_text']:
+                kwargs['label__icontains'] = request.GET['project_text']
 
             if 'fiscal_year' in request.GET and request.GET['fiscal_year']:
                 kwargs['projectinfo__fiscal_year'] = request.GET['fiscal_year']
@@ -55,22 +53,32 @@ def agency_search_report(request):
             if 'department' in request.GET and request.GET['department']:
                 kwargs['projectinfo__department'] = request.GET['department']
 
-            search_results = Project.objects.filter(**kwargs).order_by('agency__name')
-            context['results'] = search_results[:100]
+            if 'date_from' in request.GET and request.GET['date_from']:
+                date_from = datetime.datetime.strptime(request.GET['date_from'], '%m/%d/%Y')
+                kwargs['datetime_created__gte'] = date_from.replace(tzinfo=localtz)
 
-            if not context['results']:
+            if 'date_to' in request.GET and request.GET['date_to']:
+                date_to = datetime.datetime.strptime(request.GET['date_to'], '%m/%d/%Y')
+                kwargs['datetime_created__lte'] = date_to.replace(tzinfo=localtz)
+
+            search_results = Project.objects.filter(**kwargs).order_by('agency__name')
+
+            if not search_results:
                 context['message'] = _(u'No results')
             else:
-                if len(search_results) <= 100:
-                    context['message'] = string_concat(str(len(context['results'])), ' ', _(u'results'))
-                else:
-                    context['message'] = string_concat(_(u'Presenting'), ' ', str(len(context['results'])), ' ', _(u'of'), ' ', str(len(search_results)), ' ', _(u'results, please use a filter'))
+                paginator = Paginator(search_results, 25)
+                page = request.GET.get('page')
+                try:
+                    context['results'] = paginator.page(page)
+                except PageNotAnInteger:
+                    context['results'] = paginator.page(1)
+                except EmptyPage:
+                    context['results'] = paginator.page(paginator.num_pages)
 
         context['agency_search_form'] = form
     else:
         context.update({
             'agency_search_form': AgencySearchForm(),
-            'title': title,
         })
 
     return render_to_response('agency_search.html', context, context_instance=RequestContext(request))
